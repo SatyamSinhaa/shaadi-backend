3b7e36e998c26c46d115667de3d524ee00e35a88
Satyam Sinha <satyamsinha72@gmail.com>
2025-12-18 13:00:47 +0530

    dev web socket 3

--- TODO.md
+++
@@ -1,7 +0,0 @@
-# TODO: Update all Controllers to use ResponseEntity and return proper error messages
-
-- [x] Update UserController endpoints to use ResponseEntity and return error messages
-- [x] Update AdminController endpoints to use ResponseEntity and return error messages
-- [x] Update ChatController endpoints to use ResponseEntity and return error messages
-- [x] Update PlanController endpoints to use ResponseEntity and return error messages
-- [x] Test all endpoints to verify error messages are returned properly
--- src/main/java/com/shaadi/config/WebSocketConfig.java
+++ src/main/java/com/shaadi/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.shaadi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,7 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic", "/queue")
-              .setHeartbeatValue(new long[] {10000, 10000}); // 10 second heartbeat to keep connections alive
+              .setHeartbeatValue(new long[] {10000, 10000}) // 10 second heartbeat to keep connections alive
+              .setTaskScheduler(heartBeatScheduler()); // Required for heartbeat
         config.setApplicationDestinationPrefixes("/app");
     }
 
@@ -22,5 +26,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
+    }
+
+    @Bean
+    public TaskScheduler heartBeatScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("ws-heartbeat-");
+        scheduler.initialize();
+        return scheduler;
     }
 }
